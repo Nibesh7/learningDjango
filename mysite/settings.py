7289fe5c29116ce15534d8f0449a2a7afa3c08bd
nibesh7 <ranjitnibesh7@gmail.com>
2022-06-13 10:45:54 +0545

    loguout fucntionality added and redirect to product page after login

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from modulefinder import LOAD_CONST
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = 'myapp:products'
